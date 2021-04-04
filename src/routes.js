9a3cf04ef1d9fc4e954b20c25d5459e2e9fbf75f
boburDev <negmatov1907@gmail.com>
2021-04-05 04:44:23 +0500

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import Dashboard from "views/Dashboard.js";
+import TableList from "views/TableList.js";
+import UserProfile from "views/UserProfile.js";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "tim-icons icon-chart-pie-36",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    icon: "tim-icons icon-single-02",
+    component: UserProfile,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "Table List",
+    icon: "tim-icons icon-puzzle-10",
+    component: TableList,
+    layout: "/admin",
+  }
+];
+export default routes;
---
+++ src/views/TableList.js
@@ -0,0 +1,108 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col,
+} from "reactstrap";
+import axios from 'axios'
+import { useEffect, useState } from "react";
+
+
+function Tables() {
+
+	const [admins, setAdmins] = useState([])
+	const [students, setStudents] = useState([])
+
+	useEffect(()=> {
+		;(async()=>{
+			try {
+				const res = await axios.get('http://192.168.43.4:4000/')
+				const data = res.data
+
+				setAdmins(data.admins)
+				setStudents(data.users)
+			} catch(err) {
+				console.log(err)
+			}
+		})()
+   },[])
+
+   useEffect(()=>{
+	   console.log(admins);
+   },[admins])
+
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col md="12">
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h4">Admins Table</CardTitle>
+                <p className="category">Here we can see admis position and spec</p>
+              </CardHeader>
+              <CardBody>
+                <Table className="tablesorter" responsive>
+                  <thead className="text-primary">
+				  	<tr>
+                      <th>Name</th>
+                      <th>empty</th>
+                      <th>empty</th>
+                      <th>empty</th>
+                      <th className="text-center">status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+				  {
+					admins.map((item, key)=><tr key={key}>
+						<td>{item.admin_name}</td>
+						<td>empty</td>
+						<td>empty</td>
+						<td>empty</td>
+						<td className="text-center">{item.admin_status}</td>
+					</tr>)
+					}
+                  </tbody>
+                </Table>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md="12">
+            <Card className="card-plain">
+            <CardHeader>
+                <CardTitle tag="h4">Students Table</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <Table className="tablesorter" responsive>
+                  <thead className="text-primary">
+					<tr>
+                      <th>Name</th>
+                      <th>Phone</th>
+                      <th>Lesson</th>
+                      <th className="text-center">group</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+					{
+						students.map((item, key)=> <tr key={key}>
+							<td>{`${item.user_name}  ${item.user_surname} ${item.user_father_name}`}</td>
+							<td>{item.user_number}</td>
+							<td>{item.user_lesson}</td>
+							<td className="text-center">{item.user_group}</td>
+						</tr>)
+					}
+                  </tbody>
+                </Table>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Tables;
